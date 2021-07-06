0e1b15a6d586438f7c2adf4ee3229ac9f9e1e862
Luna Sarmiento <selene942008@hotmail.com>
2021-07-06 15:48:12 +0200

    Navbar done

---
+++ bookstore/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNavBar from "./Components/MyNavBar"
+import MyFooter from "./Components/MyFooter"
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavBar />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ bookstore/src/Components/MyFooter.jsx
@@ -0,0 +1,13 @@
+const MyFooter = () => (
+    <footer>
+        <div className="row">
+
+            <div className="col-12">
+                <p className="text-center">Copyright©</p>
+            </div>
+
+        </div>
+    </footer>
+)
+
+export default MyFooter
